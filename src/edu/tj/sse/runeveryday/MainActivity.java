529e577447c95d606a07a1b8f90d76f5e6ed2e29
Charles Hu <twolittlecats@163.com>
2014-04-02 16:03:34 +0800

    drawer v0.1

--- src/edu/tj/sse/runeveryday/MainActivity.java
+++ src/edu/tj/sse/runeveryday/MainActivity.java
@@ -1,15 +1,35 @@
 package edu.tj.sse.runeveryday;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
+public class MainActivity extends Activity {
+	private LayoutInflater drawerInflater;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//drawerInflater = getLayoutInflater();
+		createSlidingMenu();
+	}
+	
+	private void createSlidingMenu() {
+		SlidingMenu menu = new SlidingMenu(getApplicationContext());
+		menu.setMode(SlidingMenu.LEFT);
+		menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+		menu.setShadowWidthRes(R.dimen.shadow_width);
+		menu.setShadowDrawable(R.drawable.shadow);
+		menu.setFadeDegree(0.35f);
+		menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);// SlidingMenu划出时主页面显示的剩余宽度
+		// menu.setBehindWidth(400);//设置SlidingMenu菜单的宽度
+		menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+		menu.setMenu(R.layout.drawer);
 	}
 
 	@Override
