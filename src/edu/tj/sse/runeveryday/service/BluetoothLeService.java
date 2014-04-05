9af9b88239b2d13e52e60417f31558d10ae95c96
Charles Hu <twolittlecats@163.com>
2014-04-05 16:41:09 +0800

    bluetooth service v0.01

---
+++ src/edu/tj/sse/runeveryday/service/BluetoothLeService.java
@@ -0,0 +1,14 @@
+package edu.tj.sse.runeveryday.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BluetoothLeService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return null;
+	}
+
+}
