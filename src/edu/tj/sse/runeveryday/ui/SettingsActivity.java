a7c6527eb5a62c84dbb7e13a806b265b13e0a9a6
HappyCup <1274474220qq.com>
2014-05-07 17:41:49 +0800

    设置界面相关代码、xml、及新增string

--- src/edu/tj/sse/runeveryday/ui/SettingsActivity.java
+++ src/edu/tj/sse/runeveryday/ui/SettingsActivity.java
@@ -1,15 +1,24 @@
 package edu.tj.sse.runeveryday.ui;
 
 import edu.tj.sse.runeveryday.R;
-import android.app.Activity;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends PreferenceActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_settings);
+//		setContentView(R.layout.activity_settings);
+		addPreferencesFromResource(R.xml.activity_settings);
 		
+	}
+	
+	@Override
+	public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+			Preference preference) {
+		return false;
 	}
 }
