44f74c5bffa1be02b0d46b9a4d378c1b50c0c263
ShangChaoqun <548814145@qq.com>
2014-06-19 22:55:45 +0800

    Signed-off-by: ShangChaoqun <548814145@qq.com>

--- src/edu/tj/sse/runeveryday/test/AchiementActivityTest.java
+++ src/edu/tj/sse/runeveryday/test/AchiementActivityTest.java
@@ -1,12 +1,11 @@
 package edu.tj.sse.runeveryday.test;
+
 import edu.tj.sse.runeveryday.R;
 import edu.tj.sse.runeveryday.ui.MainActivity;
 import android.content.Intent;
 import android.test.InstrumentationTestCase;
 import android.test.TouchUtils;
 import android.widget.ImageView;
-
-  
 
 public class AchiementActivityTest extends InstrumentationTestCase {
 
@@ -18,51 +17,52 @@ public class AchiementActivityTest extends InstrumentationTestCase {
 			R.id.Achieve_distance_3, R.id.Achieve_distance_4,
 			R.id.Achieve_time_1, R.id.Achieve_time_2, R.id.Achieve_time_3,
 			R.id.Achieve_time_4 };
-    @Override
 
-    protected void setUp()  {
+	@Override
+	protected void setUp() {
 
-        try {
-            super.setUp();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        Intent intent = new Intent();
-
-        intent.setClassName("edu.tj.sse.runeveryday", MainActivity.class.getName());
-
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        main = (MainActivity) getInstrumentation().startActivitySync(intent);
-    }
-
-    @Override
-
-    protected void tearDown()  {
-
-        main.finish();
-
-        try {
-
-            super.tearDown();
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-
-        }
-
-    }  
-    public void test_trophy()
-    {
-    	for (int i = 0; i < 12; i++) {
-    	ImageView imageView = (ImageView ) main.findViewById(Trophy[0]);
-		assertNotNull(imageView);
+		try {
+			super.setUp();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-    }
-    public void test_click_imageview() {
-    	ImageView imageview  =(ImageView)main.findViewById(R.id.main_button);
-    	assertNotNull(imageview);
-        TouchUtils.clickView(this, imageview);
+
+		Intent intent = new Intent();
+
+		intent.setClassName("edu.tj.sse.runeveryday",
+				MainActivity.class.getName());
+
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		main = (MainActivity) getInstrumentation().startActivitySync(intent);
+	}
+
+	@Override
+	protected void tearDown() {
+
+		main.finish();
+
+		try {
+
+			super.tearDown();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
+	public void test_trophy() {
+		for (int i = 0; i < 12; i++) {
+			ImageView imageView = (ImageView) main.findViewById(Trophy[0]);
+			assertNotNull(imageView);
+		}
+	}
+
+	public void test_click_imageview() {
+		ImageView imageview = (ImageView) main.findViewById(R.id.main_button);
+		assertNotNull(imageview);
+		TouchUtils.clickView(this, imageview);
 	}
 }
--- src/edu/tj/sse/runeveryday/test/MainActivityTest.java
+++ src/edu/tj/sse/runeveryday/test/MainActivityTest.java
@@ -1,4 +1,5 @@
 package edu.tj.sse.runeveryday.test;
+
 import edu.tj.sse.runeveryday.R;
 import edu.tj.sse.runeveryday.database.business.PlanBase;
 import edu.tj.sse.runeveryday.database.entity.Training;
@@ -10,90 +11,89 @@ import android.test.TouchUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-  
-
 public class MainActivityTest extends InstrumentationTestCase {
 
 	private MainActivity main = null;
 	private PlanBase planBase;
 	private Training currentTraining;
-	public  static class AppContext extends Application {
+
+	public static class AppContext extends Application {
 		private static AppContext instance;
 
 		public static AppContext getInstance() {
 			return instance;
 		}
-	    @Override
-	    public void onCreate() {
-	        // TODO Auto-generated method stub
-	        super.onCreate();
-	        instance = this;
-	    }
+
+		@Override
+		public void onCreate() {
+			// TODO Auto-generated method stub
+			super.onCreate();
+			instance = this;
+		}
 	}
 
-    @Override
+	@Override
+	protected void setUp() {
 
-    protected void setUp()  {
+		try {
 
-        try {
+			super.setUp();
 
-            super.setUp();
+		} catch (Exception e) {
 
-        } catch (Exception e) {
+			e.printStackTrace();
 
-            e.printStackTrace();
+		}
 
-        }
+		Intent intent = new Intent();
 
-       Intent intent = new Intent();
+		intent.setClassName("edu.tj.sse.runeveryday",
+				MainActivity.class.getName());
 
-        intent.setClassName("edu.tj.sse.runeveryday", MainActivity.class.getName());
-
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        main = (MainActivity) getInstrumentation().startActivitySync(intent);
-        planBase = new PlanBase(main);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		main = (MainActivity) getInstrumentation().startActivitySync(intent);
+		planBase = new PlanBase(main);
 		currentTraining = planBase.getCurrentTraining();
-    }
+	}
 
-    @Override
+	@Override
+	protected void tearDown() {
 
-    protected void tearDown()  {
+		main.finish();
 
-        main.finish();
+		try {
 
-        try {
+			super.tearDown();
 
-            super.tearDown();
+		} catch (Exception e) {
 
-        } catch (Exception e) {
+			e.printStackTrace();
 
-            e.printStackTrace();
+		}
 
-        }
+	}
 
-    }
-    
-   public void test_temperature() throws Throwable {  
-    	TextView textView  =(TextView)main.findViewById(R.id.Main_tempreture);
-    	assertNotNull(textView);
-    	assertEquals("20¡ã", textView.getText());
-    }  
-    
-    public void test_humidity() throws Throwable {  
-    	TextView textView  =(TextView)main.findViewById(R.id.Main_shidu);
-    	assertNotNull(textView);
-    	assertEquals("Êª¶È   50%", textView.getText());
-    } 
-    
-    public void test_plan() throws Throwable {  
-    	TextView textView  =(TextView)main.findViewById(R.id.Main_content);
-    	assertNotNull(textView);
-    	assertEquals(currentTraining.getWork(), textView.getText());
-    } 
-    
-    public void test_click_imageview() {
-    	ImageView imageview  =(ImageView)main.findViewById(R.id.main_button);
-    	assertNotNull(imageview);
-        TouchUtils.clickView(this, imageview);
+	public void test_temperature() throws Throwable {
+		TextView textView = (TextView) main.findViewById(R.id.Main_tempreture);
+		assertNotNull(textView);
+		assertEquals("20¡ã", textView.getText());
+	}
+
+	public void test_humidity() throws Throwable {
+		TextView textView = (TextView) main.findViewById(R.id.Main_shidu);
+		assertNotNull(textView);
+		assertEquals("Êª¶È   50%", textView.getText());
+	}
+
+	public void test_plan() throws Throwable {
+		TextView textView = (TextView) main.findViewById(R.id.Main_content);
+		assertNotNull(textView);
+		assertEquals(currentTraining.getWork(), textView.getText());
+	}
+
+	public void test_click_imageview() {
+		ImageView imageview = (ImageView) main.findViewById(R.id.main_button);
+		assertNotNull(imageview);
+		TouchUtils.clickView(this, imageview);
 	}
 }
